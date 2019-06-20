0d9260af8578d298ddcc7169769461f97dbca5e2
João Antônio Cardoso <joao.maker@gmail.com>
2019-06-20 02:46:37 -0300

    license header added

--- array2d_example.c
+++ array2d_example.c
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the C Array Macros distribution (https://github.com/joaoantoniocardoso/c_array_macros).
+ * Copyright (c) 2019 João Antônio Cardoso.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include "array_macros.h"
 
